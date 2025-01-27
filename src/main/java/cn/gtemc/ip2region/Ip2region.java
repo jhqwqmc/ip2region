33b27c7e592c5b7e0c3e5ee94dd0114c505c1467
jhqwqmc <2110242767@qq.com>
2025-01-27 11:54:30 +0800

    添加PlaceholderAPI依赖

--- src/main/java/cn/gtemc/ip2region/Ip2region.java
+++ src/main/java/cn/gtemc/ip2region/Ip2region.java
@@ -1,17 +1,21 @@
 package cn.gtemc.ip2region;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ip2region extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        if (!Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            getLogger().severe("未安装PlaceholderAPI无法启动");
+            this.onDisable();
+        }
+        getLogger().info("插件已启用");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("插件已禁用");
     }
 }
