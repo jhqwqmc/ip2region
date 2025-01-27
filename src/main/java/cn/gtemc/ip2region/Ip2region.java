5e2f6c1c0c425261ee3900b4b5de0cb26ab6a6e1
jhqwqmc <2110242767@qq.com>
2025-01-27 12:15:57 +0800

    添加ip2region依赖

--- src/main/java/cn/gtemc/ip2region/Ip2region.java
+++ src/main/java/cn/gtemc/ip2region/Ip2region.java
@@ -2,8 +2,13 @@ package cn.gtemc.ip2region;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.lionsoul.ip2region.xdb.Searcher;
+
+import java.io.IOException;
 
 public final class Ip2region extends JavaPlugin {
+    public static Ip2region instance;
+    public static Searcher searcher;
 
     @Override
     public void onEnable() {
@@ -11,11 +16,25 @@ public final class Ip2region extends JavaPlugin {
             getLogger().severe("未安装PlaceholderAPI无法启动");
             this.onDisable();
         }
+        instance = this;
+        try {
+            searcher = Searcher.newWithFileOnly(getDataFolder().getAbsolutePath() + "/ip2region.xdb");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         getLogger().info("插件已启用");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("插件已禁用");
+    }
+
+    public static Ip2region getInstance() {
+        return instance;
+    }
+
+    public static Searcher getSearcher() {
+        return searcher;
     }
 }
