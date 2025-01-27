ff6038d7cf43f33f5df5bb3df0636fbfeb9b6fff
jhqwqmc <2110242767@qq.com>
2025-01-27 11:47:52 +0800

    初始化

---
+++ src/main/java/cn/gtemc/ip2region/Ip2region.java
@@ -0,0 +1,17 @@
+package cn.gtemc.ip2region;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ip2region extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
